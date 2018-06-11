aa4cb70ba9736cc57ffdc6b3940d73d8aae67802
Gregor von Laszewski <laszewski@gmail.com>
2018-06-11 19:11:45 -0400

    improve merge

--- cloudmesh/openapi/bin/merge-openapi.py
+++ cloudmesh/openapi/bin/merge-openapi.py
@@ -1,5 +1,5 @@
 import click
-
+import re
 import os
 
 def read(dir, service):
@@ -7,27 +7,52 @@ def read(dir, service):
         content = f.read()
     return content
 
+def read_header(dir, service):
+    with open(os.path.join(dir, service +".yaml"), "r") as f:
+        content = f.read()
+    return content
+
 def parse_definitions(content):
-    print (content.split("definitions:")[1])
+    return content.split("definitions:")[1]
 
 def parse_paths(content):
-    print(content.split("paths:")[1].split("definitions:")[0])
+    return content.split("paths:")[1].split("definitions:")[0]
+
+
+def merge_yaml(services):
+    out = ""
+    out = out + "definitions:"
+    for service in services:
+        content = read("services", service)
+        out = out + parse_paths (content)
+
+    out = out + "paths:"
+    for service in services:
+        content = read("services", service)
+        out = out + parse_definitions (content)
+    return out
 
 
 
 @click.command()
 @click.argument('services', nargs=-1)
 def merge(services):
-    print ("definitions:")
-    for service in services:
-        content = read("services", service)
-        parse_paths (content)
 
-    print ("paths:")
+    header = read_header("service","header")
+    out = header + "definitions:"
+
     for service in services:
         content = read("services", service)
-        parse_definitions (content)
+        out = out + parse_paths (content)
+
+    out = out + "paths:"
+    for service in services:
+        content = read("services", service)
+        out = out + parse_definitions (content)
+    out = re.sub(r'\n+', '\n', out)
+    print(out)
 
 
 if __name__ == '__main__':
+
     merge()
