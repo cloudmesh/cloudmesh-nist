74d1ee8e9d8b37bb53c245e95e2bc48e96b814f1
fugangwang <kevinwangfg@gmail.com>
2018-06-11 13:55:26 -0400

    changing adding profile to use put instead of using get

--- cloudmesh/profile/default_controller.py
+++ cloudmesh/profile/default_controller.py
@@ -1,7 +1,9 @@
 import connexion
 import six
+import uuid
+
 #from profile_controller import *
-from swagger_server.models.profile import PROFILE  # noqa: E501
+from swagger_server.models.profile import Profile  # noqa: E501
 from swagger_server import util
 from pymongo import MongoClient
 # Connect to the MongoDB, change the connection string per your MongoDB environment
@@ -19,7 +21,7 @@ def get_profile_by_uuid(uuid):
     :rtype: PROFILE
     """
     item = get_profile_by_uuid_mongo(uuid)
-    return PROFILE(item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7])
+    return Profile(item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7])
 def profiles_get():  # noqa: E501
     """profiles_get
 
@@ -30,7 +32,7 @@ def profiles_get():  # noqa: E501
     listOfProfile = []
     items = get_profile()
     for each in items:
-        listOfProfile.append(PROFILE(each[0],each[1],each[2],each[3],each[4],each[5],each[6],each[7]))
+        listOfProfile.append(Profile(each[0],each[1],each[2],each[3],each[4],each[5],each[6],each[7]))
     return listOfProfile
 
 def get_profile():
@@ -47,6 +49,10 @@ def add_profile_mongo(uuid, username,context,description,firstname, lastname,pub
 	db.Profile.insert({"uuid":uuid, "username":username,"context":context, "description":description, "firstname":firstname, "lastname":lastname, "publickey":publickey,"email":"yy@iu.edu"})
 	return "add a new profile successfully"
 
-
-def add_profile(uuid, username,context,description,firstname, lastname,publickey, email):
-	return PROFILE(uuid,username,context,description,firstname,lastname,publickey,email)
+def add_profile(profile=None):
+    print (profile)
+    uuid = str(uuid.uuid4())
+    if connexion.request.is_json:
+        profile = Profile.from_dict(connexion.request.get_json())
+    print (profile)
+    return profile
