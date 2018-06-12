3702172c558eefd1e5439645678c60841229caeb
Gregor von Laszewski <laszewski@gmail.com>
2018-06-12 11:03:16 -0400

    remove the linebreaks in curl as they seem not to work

--- cloudmesh/openapi/services/profile/test/test_profile.sh
+++ cloudmesh/openapi/services/profile/test/test_profile.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
-curl -H 'Content-Type: application/json' -X PUT -d \
-	   '{"usename": "testname", \
-	   "firstname": "John", \
-	   "lastname": "Doe", \
-	   "publickey": "ssh-rsa:xxx", \
-	   "email": "user@exmaple.com"}' \
-	   http://localhost:8080/cloudmesh/profile/profiles
+
+curl -H 'Content-Type: application/json' -X PUT -d '{"usename": "testname", "firstname": "John", "lastname": "Doe", "publickey": "ssh-rsa:xxx", "email": "user@exmaple.com"}' http://localhost:8080/cloudmesh/profile/profiles
+
 curl http://localhost:8080/cloudmesh/profile/profiles
