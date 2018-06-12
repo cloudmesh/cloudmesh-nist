1e438628fcbd60005472254ea0d2a27f13d03b72
Gregor von Laszewski <laszewski@gmail.com>
2018-06-12 10:59:49 -0400

    add bash in script

--- cloudmesh/openapi/services/profile/test/test_profile.sh
+++ cloudmesh/openapi/services/profile/test/test_profile.sh
@@ -1,8 +1,9 @@
-	curl -H 'Content-Type: application/json' -X PUT -d \
+#! /bin/bash
+curl -H 'Content-Type: application/json' -X PUT -d \
 	   '{"usename": "testname", \
 	   "firstname": "John", \
 	   "lastname": "Doe", \
 	   "publickey": "ssh-rsa:xxx", \
 	   "email": "user@exmaple.com"}' \
 	   http://localhost:8080/cloudmesh/profile/profiles
-	curl http://localhost:8080/cloudmesh/profile/profiles
+curl http://localhost:8080/cloudmesh/profile/profiles
