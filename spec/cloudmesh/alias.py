6b8e5f2e6f3934e0a888ef2652c456beb553f99a
Gregor von Laszewski <laszewski@gmail.com>
2019-05-26 09:16:46 -0400

    conversion to OpenAPI 3.0.2

--- spec/cloudmesh/alias.py
+++ spec/cloudmesh/alias.py
@@ -1,28 +1,28 @@
 def get():
-  '''Returns all aliases
+  """Returns all aliases
 
 
-  '''
+  """
 
-  return 'TO BE IMPLEMENTED'
+  return "TO BE IMPLEMENTED"
 
 def put(alias):
-  '''Create a new alias
+  """Create a new alias
 
   Params:
     alias - The new alias to create
 
-  '''
+  """
 
-  return 'TO BE IMPLEMENTED'
+  return "TO BE IMPLEMENTED"
 
 def get_by_name(name):
-  '''Returns an alias by name
+  """Returns an alias by name
 
   Params:
     name - The name of the alias
 
-  '''
+  """
 
-  return 'TO BE IMPLEMENTED'
+  return "TO BE IMPLEMENTED"
 
--- spec/cloudmesh/user.py
+++ spec/cloudmesh/user.py
@@ -1,28 +1,28 @@
 def get():
-  '''Returns all users
+  """Returns all users
 
 
-  '''
+  """
 
-  return 'TO BE IMPLEMENTED'
+  return "TO BE IMPLEMENTED"
 
 def put(user):
-  '''Create a new user
+  """Create a new user
 
   Params:
     user - The new user to create
 
-  '''
+  """
 
-  return 'TO BE IMPLEMENTED'
+  return "TO BE IMPLEMENTED"
 
 def get_by_name(name):
-  '''Returns the user of a user while looking it up with the username
+  """Returns the user of a user while looking it up with the username
 
   Params:
     name - name of the user
 
-  '''
+  """
 
-  return 'TO BE IMPLEMENTED'
+  return "TO BE IMPLEMENTED"
 
