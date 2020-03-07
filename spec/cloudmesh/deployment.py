4fd64f45584134901ccb03928a57cb0f240970ab
Gregor von Laszewski <laszewski@gmail.com>
2020-03-07 11:15:59 -0500

    improve version management

---
+++ spec/cloudmesh/deployment.py
@@ -0,0 +1,36 @@
+def list():
+  '''Returns a list of deploymentes
+
+
+  '''
+
+  return 'TO BE IMPLEMENTED'
+
+def add():
+  '''Set an deployment
+
+
+  '''
+
+  return 'TO BE IMPLEMENTED'
+
+def find_by_name(name):
+  '''Returns the named deployment
+
+  Params:
+    name - The name of the deployment
+
+  '''
+
+  return 'TO BE IMPLEMENTED'
+
+def delete_by_name(name):
+  '''Deletes the named deployment
+
+  Params:
+    name - The name of the deployment
+
+  '''
+
+  return 'TO BE IMPLEMENTED'
+
